b2498738b848975da61b9f4805d2f518bd44ac43
mgegner <info@maltegegner.de>
2016-09-05 22:10:22 +0200

    +Created styleFetcher library.

---
+++ styleFetcher.js
@@ -0,0 +1,97 @@
+var styleFetcher = styleFetcher || (function() {
+	
+	var _args = {
+		/* Configuration */
+		styleURL: "https://raw.githubusercontent.com/mgegner/local-style-fetcher/master/example-style.css",
+		removeStylesOnStart: true,
+		updateLinkTagsOnStart: true,
+		reloadStyleTime: 2000,
+	};
+	
+	return {
+		oldTimestamp: (new Date()).getTime(),
+		timeStamp: (new Date()).getTime(),
+		
+		removeStyle: function() {
+			removeStyles = function(el) {
+				el.removeAttribute('style'); 
+				if(el.childNodes.length > 0){
+					for(var child in el.childNodes){
+						if(el.childNodes[child].nodeType == 1){
+							removeStyles(el.childNodes[child]);
+						}
+					}
+				}
+			};
+			removeStyles(document.body);
+			for(var i = 0; i < document.styleSheets.length; i++){ 
+				document.styleSheets[i].disabled = true;
+			}
+		},
+		
+		updateLinkTags: function() {
+			var head = document.getElementsByTagName("head")[0];
+			var links = head.getElementsByTagName("link");
+			var i = 0;
+			while(i < links.length) {
+				if(links[i].nodeType == 1 && links[i].rel == "stylesheet") {
+					head.removeChild(links[i]);
+				} else {
+					i++;
+				}
+			};
+			timeStamp = (new Date()).getTime();
+			var newStyle = document.createElement("link");
+			newStyle.type = "text/css";
+			newStyle.rel = "stylesheet";
+			newStyle.id = "newStyle-"+timeStamp;
+			newStyle.href = _args.styleURL + "?time=" + timeStamp;
+			document.getElementsByTagName("head")[0].appendChild(newStyle);
+		},
+		
+		removeOldNewStyle: function() {
+			links = document.getElementsByTagName("head")[0].getElementsByTagName("link");
+			for(var i = 0; i < links.length; i++) {
+				if(links[i].nodeType == 1 && links[i].id == "newStyle-"+oldTimestamp) {
+					document.getElementsByTagName("head")[0].removeChild(links[i]);
+					i = 1000;
+				}
+			}
+		},
+		
+		reloadNewStyle: function() {
+			oldTimestamp = timeStamp;
+			timeStamp = (new Date()).getTime();
+			var newStyle = document.createElement("link");
+			newStyle.type = "text/css";
+			newStyle.rel = "stylesheet";
+			newStyle.id = "newStyle-"+timeStamp;
+			newStyle.href = _args.styleURL + "?time=" + timeStamp;
+			document.getElementsByTagName("head")[0].appendChild(newStyle);
+			setTimeout(this.removeOldNewStyle, 1000);
+		},
+		
+		init: function(Args) {
+			if(!!Args) {
+				if(Args.styleURL) { _args.styleURL = Args.styleURL; }
+				if(Args.removeStylesOnStart) { _args.removeStylesOnStart = Args.removeStylesOnStart; }
+				if(Args.updateLinkTagsOnStart) { _args.updateLinkTagsOnStart = Args.updateLinkTagsOnStart; }
+				if(Args.reloadStyleTime) { _args.reloadStyleTime = Args.reloadStyleTime; }
+			}
+			
+			if(!!_args.removeStylesOnStart) {
+				this.removeStyle();
+			}
+			if(!!_args.updateLinkTagsOnStart) {
+				this.updateLinkTags();
+			}
+			if(_args.reloadStyleTime > 0) {
+				setInterval(this.reloadNewStyle, _args.reloadStyleTime);
+			} else {
+				this.reloadNewStyle();
+			}
+		}
+		
+	};
+	
+})();
